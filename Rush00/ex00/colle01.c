8362677af0c426ed26e35314b6a79a2dee4daf37
mirceap <pavelmircea92@gmail.com>
2017-09-03 05:42:53 +0300

    Rush00 Added

---
+++ Rush00/ex00/colle01.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colle00.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhudema <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/03 16:21:46 by dhudema           #+#    #+#             */
+/*   Updated: 2016/09/03 17:56:48 by dhudema          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	print_line(char *template, int y);
+
+void	init_variables(char *h, char *c, char *f)
+{
+	h[0] = '/';
+	h[1] = '*';
+	h[2] = '\\';
+	c[0] = '*';
+	c[1] = ' ';
+	c[2] = '*';
+	f[0] = '\\';
+	f[1] = '*';
+	f[2] = '/';
+}
+
+void	colle(int x, int y)
+{
+	char	header[3];
+	char	content[3];
+	char	footer[3];
+	int		i;
+
+	init_variables(header, content, footer);
+	if (x < 1 || y < 1)
+		return ;
+	print_line(header, x);
+	i = 2;
+	while (i < y)
+	{
+		print_line(content, x);
+		i++;
+	}
+	if (y != 1)
+		print_line(footer, x);
+}
+
+void	print_line(char *template, int x)
+{
+	int j;
+
+	ft_putchar(template[0]);
+	j = 2;
+	while (j < x)
+	{
+		ft_putchar(template[1]);
+		j++;
+	}
+	if (x != 1)
+		ft_putchar(template[2]);
+	ft_putchar('\n');
+}
---
+++ Rush00/ex00/colle02.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colle00.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhudema <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/03 16:21:46 by dhudema           #+#    #+#             */
+/*   Updated: 2016/09/03 18:00:26 by dhudema          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	print_line(char *template, int y);
+
+void	init_variables(char *h, char *c, char *f)
+{
+	h[0] = 'A';
+	h[1] = 'B';
+	h[2] = 'A';
+	c[0] = 'B';
+	c[1] = ' ';
+	c[2] = 'B';
+	f[0] = 'C';
+	f[1] = 'B';
+	f[2] = 'C';
+}
+
+void	colle(int x, int y)
+{
+	char	header[3];
+	char	content[3];
+	char	footer[3];
+	int		i;
+
+	init_variables(header, content, footer);
+	if (x < 1 || y < 1)
+		return ;
+	print_line(header, x);
+	i = 2;
+	while (i < y)
+	{
+		print_line(content, x);
+		i++;
+	}
+	if (y != 1)
+		print_line(footer, x);
+}
+
+void	print_line(char *template, int x)
+{
+	int j;
+
+	ft_putchar(template[0]);
+	j = 2;
+	while (j < x)
+	{
+		ft_putchar(template[1]);
+		j++;
+	}
+	if (x != 1)
+		ft_putchar(template[2]);
+	ft_putchar('\n');
+}
